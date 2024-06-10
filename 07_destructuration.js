3b0c17f18fce7fba3ebb60078b71315571f9a62c
HIMIDI Ahmed <nassor.devweb@gmail.com>
2024-06-10 13:57:15 +0200

    exo8

--- 07_destructuration.js
+++ 07_destructuration.js
@@ -12,7 +12,9 @@
  *  - interdiction d'utiliser slice ou splice
  */
 
-const extractFirstTwo = () => {}
+const extractFirstTwo = ([a,b,...rest]) => {
+    return [a,b]
+}
 
 /**
  * utilisez la décomposition pour extraire la premiere case du tableau et le reste du tableau
@@ -21,7 +23,9 @@ const extractFirstTwo = () => {}
  * exemple: [1, 2, 3] => [2, 3]
  */
 
-const extractRest = () => {}
+const extractRest = ([a,...rest]) => {
+    return rest
+}
 
 /**
  * utilisez la décomposition pour extraire le champ "name" de l'objet passé en paramètre
